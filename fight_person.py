a848c0df4fb9d5b6edab3c67ec55dcc97f6b9881
pinghoyk <pinghoyk009@gmail.com>
2024-08-07 20:49:32 +0500

    улучшение скрипт драки

--- fight_person.py
+++ fight_person.py
@@ -1,18 +1,20 @@
 import requests
+import json
 
-# Замените INSERT_TOKEN_HERE на ваш реальный токен
-token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6IkZhbGJ1ZSIsInBhc3N3b3JkX2NoYW5nZWQiOiIifQ.rgxtPlIeKnAd3E7RKTdSTv60oLbVN5BqeOks8hmqxpk"
-character_name = "Falbue"
+# ПЕРМЕННЫЕ----------------------------
+# конфиг
+with open('config.json', 'r') as config_file:
+    config = json.load(config_file)
 
-# Заголовки запроса
-headers = {
-    "Accept": "application/json",
-    "Content-Type": "application/json",
-    "Authorization": f"Bearer {token}"
-}
+TOKEN = config['token']
+HEADERS = config['headers']
+
+
+# переменные скрипта
+PLAYER = "Falbue" # имя персонажа
 
 # Выполнение запроса
-response = requests.post(f"https://api.artifactsmmo.com/my/{character_name}/action/fight", headers=headers)
+response = requests.post(f"https://api.artifactsmmo.com/my/{PLAYER}/action/fight", headers=headers)
 
 # Обработка ответа
 if response.status_code == 200:
