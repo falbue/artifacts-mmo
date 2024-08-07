38280ef0be58a98188fda3f31bf2bbc31e4a456c
pinghoyk <pinghoyk009@gmail.com>
2024-08-07 21:51:00 +0500

    создание скрипта на получение ресурсов

---
+++ gathering.py
@@ -0,0 +1,25 @@
+# БИБЛИОТЕКИ----------------------------
+import requests
+import json
+from http_status import http_status
+
+# ПЕРМЕННЫЕ----------------------------
+# конфиг
+with open('config.json', 'r') as config_file:
+    config = json.load(config_file)
+
+TOKEN = config['token']
+HEADERS = config['headers']
+URL = config['url']
+
+# переменные скрипта
+PLAYER = "Falbue" # имя персонажа
+
+
+# Выполнение запроса
+response = requests.post(f"{URL}/my/{PLAYER}/action/gathering", headers=HEADERS, json=data)
+
+# Обработка ответа
+if response.status_code == 200:
+    print("Ресурс добыт!")
+http_status(response.status_code)
