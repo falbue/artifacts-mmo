164280263ed4e62e4ed03653d8eb6077b945642d
Falbue <cyansair05@gmail.com>
2024-11-17 01:22:49 +0500

    Добавлен скрипт драки

---
+++ fight.py
@@ -0,0 +1,21 @@
+from send_request import *
+import time
+from plyer import notification
+from tg_notif import *
+from fight import *
+
+def fight(name, time_work=None):
+	i = 0
+	notification.notify(title='Задача запущена!', message='Бой начался', timeout=10)
+	while i != time_work:
+	    data = send_request(f'my/{name}/action/rest', True)  # лечение
+	    x = data['Кулдаун']["Всего секунд"]
+	    time.sleep(x)
+	    data = send_request("my/Falbue/action/fight", True) # бой
+	    x = data['Кулдаун']["Всего секунд"]
+	    time.sleep(x)
+	    print(f"Бой {i} закончен")
+	    i+=1
+    
+	notification.notify(title='Задача выполнена!', message='Бой закончен!', timeout=10)
+	tg_notif("Бой закончен!")
