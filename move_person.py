fe0c8dff230220e3f320371d82b34a9f3009b453
Falbue <cyansair05@gmail.com>
2024-11-16 22:09:04 +0500

    упрощён код

--- move_person.py
+++ move_person.py
@@ -1,34 +1,9 @@
-# БИБЛИОТЕКИ----------------------------
-import requests
-import json
-from http_status import http_status
+from send_request import send_request
 
 # ПЕРМЕННЫЕ----------------------------
-# конфиг
-with open('config.json', 'r') as config_file:
-    config = json.load(config_file)
-
-TOKEN = config['token']
-HEADERS = config['headers']
-URL = config['url']
-
-# переменные скрипта
-PLAYER = "Falbue" # имя персонажа
-
-
-# Тело запроса
 data = {
-    "x": 1,
+    "x": 9,
     "y": 1
 }
 
-# Выполнение запроса
-response = requests.post(f"{URL}/my/{PLAYER}/action/move", headers=HEADERS, json=data)
-
-
-# Обработка ответа
-if response.status_code == 200:
-    print("Персонаж успешно переместился!")
-else:
-    http_status(response.status_code)
-
+data = send_request("my/Falbue/action/move", data)
