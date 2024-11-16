65d58effdb4b0e0b666b96c872198e7ea6a20186
Falbue <cyansair05@gmail.com>
2024-11-16 23:19:40 +0500

    Удалены все лишние файлы

--- commands.py
+++ commands.py
@@ -2,11 +2,13 @@ from send_request import *
 from translate_data import *
 
 
-data = send_request('characters/Falbue')  # получение инофрмации о персонаже
+# data = send_request('characters/Falbue')  # получение инофрмации о персонаже
+data = send_request('maps/0/1')  # получение инофрмации о карте
 # data = send_request("my/Falbue/action/move", {"x":0,"y":1})  # перемещение персонажа
 # data = send_request("my/Falbue/action/fight", True) # бой
 # data = send_request("my/Falbue/action/gathering", True) # добыча
+# data = send_request("my/Falbue/action/crafting", True) # крафт
+# data = send_request("my/Falbue/action/gathering", True) # Добыча
 
 # обработка данных
-print(data)
-print_data(data["Инвентарь"])
+print_data(data)
