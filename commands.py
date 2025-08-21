2d4614682725fbacd9262d8eeacf9506afecae10
falbue <cyansair05@gmail.com>
2025-08-21 19:06:49 +0500

    Добыча указанного ресурса

--- commands.py
+++ commands.py
@@ -29,3 +29,33 @@ def scan_maps():
     
     save_file(maps_file, cache_data)
     return maps
+
+def mining_resource(character="all", resource=None, quantity=1):
+    characters = mmo_request("/my/characters")
+    names = []
+    if character == "all":
+        for i in range(len(characters["data"])):
+            names.append(characters["data"][i]["Имя"])
+    elif character <= len(characters["data"]):
+        names.append(characters["data"][character-1]["Имя"])
+    else:
+        logger.error("Персонаж не выбран. Измените параметры")
+
+    for name in names:
+        maps = scan_maps()
+        coordinats = find_objects(maps, resource)
+        cooldown = 0
+        cooldown = mmo_request(f"/my/{name}/action/move", coordinats, cooldown=True)
+        thread = threading.Thread(target=gathering, args=(name, cooldown, quantity))
+        thread.start()
+
+def gathering(name, cooldown, quantity):
+    logger.debug(f"{name} приступил к добыванию ресурса")
+    time.sleep(int(cooldown))
+    for i in range(quantity):
+        cooldown = mmo_request(f"/my/{name}/action/gathering", True, True)
+        time.sleep(cooldown)
+
+# scan_maps()
+mining_resource(character="all", resource="copper_rocks", quantity=20)
+# data = mmo_request(f"/my/Falbue/action/move", {"x":"0","y":"0"})
