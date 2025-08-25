d2588a990e02398c47a9028551678da1e663b69e
falbue <cyansair05@gmail.com>
2025-08-25 23:22:20 +0500

    Перенесены команды

--- commands.py
+++ commands.py
@@ -1,6 +1,90 @@
 from mmo_request import mmo_request
 from utils import *
 
+def scan_maps():
+    maps_file = "maps.json"
+    cache_data = load_file(maps_file)
+    if cache_data is not None:
+        if isinstance(cache_data, dict) and 'timestamp' in cache_data and 'data' in cache_data:
+            cache_time = datetime.fromisoformat(cache_data['timestamp'])
+            
+        if datetime.now() - cache_time < timedelta(hours=1):
+            logger.debug("Данные из кеша")
+            return cache_data['data']
+    
+    logger.debug("Cканирование карт...")
+    maps = []
+    data = mmo_request("/maps?size=100")
+    pages = data["pages"]
+    
+    for i in range(pages):
+        command = f"/maps?size=100&page={i+1}"
+        data = mmo_request(command)
+        maps.extend(data["data"])
+    
+    cache_data = {
+        'timestamp': datetime.now().isoformat(),
+        'data': maps
+    }
+    
+    save_file(maps_file, cache_data)
+    return maps
+
+def scan_items():
+    file = "items.json"
+    cache_data = load_file(file)
+    if cache_data is not None:
+        if isinstance(cache_data, dict) and 'timestamp' in cache_data and 'data' in cache_data:
+            cache_time = datetime.fromisoformat(cache_data['timestamp'])
+            
+        if datetime.now() - cache_time < timedelta(hours=1):
+            logger.debug("Данные из кеша")
+            return cache_data['data']
+    
+    logger.debug("Cканирование ресурсов...")
+    items = []
+    data = mmo_request("/items?size=100")
+    pages = data["pages"]
+    
+    for i in range(pages):
+        command = f"/items?size=100&page={i+1}"
+        data = mmo_request(command)
+        items.extend(data["data"])
+    
+    cache_data = {
+        'timestamp': datetime.now().isoformat(),
+        'data': items
+    }
+    
+    save_file(file, cache_data)
+    return items
+
+def find_workshop(craftable):
+    results = []
+    maps = scan_maps()
+    for item in maps:
+        if item.get("Контент"):
+            if item["Контент"].get("Код") == craftable:
+                x = item.get("x")
+                y = item.get("y")
+                if x is not None and y is not None:
+                   results.append({"x": x, "y": y})
+    return results[0]
+
+def load_characters(character):
+    characters = mmo_request("/my/characters")
+    names = []
+    if character == "all":
+        for i in range(len(characters["data"])):
+            names.append(characters["data"][i])
+    elif character <= len(characters["data"]):
+        names.append(characters["data"][character-1])
+    else:
+        logger.error("Персонаж не выбран. Измените параметры")
+    return names
+
+
+
 def gathering(name, cooldown, quantity):
     logger.debug(f"{name} приступил к добыванию ресурса")
     time.sleep(int(cooldown))
--- instructions.py
+++ instructions.py
@@ -2,122 +2,45 @@ from mmo_request import mmo_request
 from utils import *
 from commands import *
 
-def scan_maps():
-    maps_file = "maps.json"
-    cache_data = load_file(maps_file)
-    if cache_data is not None:
-        if isinstance(cache_data, dict) and 'timestamp' in cache_data and 'data' in cache_data:
-            cache_time = datetime.fromisoformat(cache_data['timestamp'])
-            
-        if datetime.now() - cache_time < timedelta(hours=1):
-            logger.debug("Данные из кеша")
-            return cache_data['data']
-    
-    logger.debug("Cканирование карт...")
-    maps = []
-    data = mmo_request("/maps?size=100")
-    pages = data["pages"]
-    
-    for i in range(pages):
-        command = f"/maps?size=100&page={i+1}"
-        data = mmo_request(command)
-        maps.extend(data["data"])
-    
-    cache_data = {
-        'timestamp': datetime.now().isoformat(),
-        'data': maps
-    }
-    
-    save_file(maps_file, cache_data)
-    return maps
-
-def scan_items():
-    file = "items.json"
-    cache_data = load_file(file)
-    if cache_data is not None:
-        if isinstance(cache_data, dict) and 'timestamp' in cache_data and 'data' in cache_data:
-            cache_time = datetime.fromisoformat(cache_data['timestamp'])
-            
-        if datetime.now() - cache_time < timedelta(hours=1):
-            logger.debug("Данные из кеша")
-            return cache_data['data']
-    
-    logger.debug("Cканирование ресурсов...")
-    items = []
-    data = mmo_request("/items?size=100")
-    pages = data["pages"]
-    
-    for i in range(pages):
-        command = f"/items?size=100&page={i+1}"
-        data = mmo_request(command)
-        items.extend(data["data"])
-    
-    cache_data = {
-        'timestamp': datetime.now().isoformat(),
-        'data': items
-    }
-    
-    save_file(file, cache_data)
-    return items
-
-def find_workshop(craftable):
-    results = []
-    maps = scan_maps()
-    for item in maps:
-        if item.get("Контент"):
-            if item["Контент"].get("Код") == craftable:
-                x = item.get("x")
-                y = item.get("y")
-                if x is not None and y is not None:
-                   results.append({"x": x, "y": y})
-    return results[0]
-
-def load_characters(character):
-    characters = mmo_request("/my/characters")
-    names = []
-    if character == "all":
-        for i in range(len(characters["data"])):
-            names.append(characters["data"][i]["Имя"])
-    elif character <= len(characters["data"]):
-        names.append(characters["data"][character-1]["Имя"])
-    else:
-        logger.error("Персонаж не выбран. Измените параметры")
-    return names
 
 def mining_resource(character="all", resource=None, quantity=1):
-    names = load_characters(character)
-    for name in names:
+    characters = load_characters(character)
+    for character in characters:
+        name = character["Имя"]
         maps = scan_maps()
-        coordinats = find_objects(maps, resource)
+        coordinates = find_objects(maps, resource)
+        if len(coordinates) > 1:
+            coordinates = nearest_object(coordinates, {"x":character["x"],"y":character["y"]})
         cooldown = 0
-        cooldown = mmo_request(f"/my/{name}/action/move", coordinats, cooldown=True)
+        cooldown = mmo_request(f"/my/{name}/action/move", coordinates, cooldown=True)
         thread = threading.Thread(target=gathering, args=(name, cooldown, quantity))
         thread.start()
 
 
 def fighting(character="all", resource=None, quantity=1):
-    names = load_characters(character)
-    for name in names:
+    characters = load_characters(character)
+    for character in characters:
+        name = character["Имя"]
         maps = scan_maps()
-        coordinats = find_objects(maps, resource)
+        coordinates = find_objects(maps, resource)
         cooldown = 0
-        cooldown = mmo_request(f"/my/{name}/action/move", coordinats, cooldown=True)
+        cooldown = mmo_request(f"/my/{name}/action/move", coordinates, cooldown=True)
         thread = threading.Thread(target=gathering, args=(name, cooldown, quantity))
         thread.start()
 
 
 def crafting(character="all", resource=None, quantity=1):
     names = load_characters(character)
-    for name in names:
+    for character in characters:
+        name = character["Имя"]
         items = scan_items()
         for item in items:
             if item.get("Код") == resource:
                 craftable = item["craft"]["skill"]
-                coordinats = find_workshop(craftable)
-        cooldown = mmo_request(f"/my/{name}/action/move", coordinats, cooldown=True)
+                coordinates = find_workshop(craftable)
+        cooldown = mmo_request(f"/my/{name}/action/move", coordinates, cooldown=True)
         thread = threading.Thread(target=craft, args=(name, resource, cooldown, quantity))
         thread.start()
 
-# scan_maps()
-# mining_resource(character="all", resource="ash_tree", quantity=1)
-crafting(character="all", resource="copper_helmet", quantity=1)
+mining_resource(character="all", resource="ash_tree", quantity=1)
+# crafting(character="all", resource="copper_helmet", quantity=1)
