29bbb9539d12b0678f43024c4e9f589738991a11
falbue <cyansair05@gmail.com>
2025-12-12 01:14:46 +0500

    Перенесена папка с локализацией

--- core/utils/request.py
+++ core/utils/request.py
@@ -2,7 +2,9 @@ import requests
 import json
 
 
-def request_mmo(command="", body=None, TOKEN="", localize="ru"):
+def request_mmo(
+    command="", body: bool | dict | None = None, TOKEN="", localize="ru"
+) -> dict:
     """ "Отправляет запрос к API Artifacts MMO и возвращает ответ в виде словаря"""
     if command.startswith("/"):
         command = command[1:]
@@ -30,7 +32,7 @@ def request_mmo(command="", body=None, TOKEN="", localize="ru"):
         error_code = response.status_code
 
         with open(
-            f"core/localize/{localize}/error.json", "r", encoding="utf-8"
+            f"core/utils/localize/{localize}/error.json", "r", encoding="utf-8"
         ) as file:
             errors = json.load(file)
         error_message = errors.get(f"{response.status_code}") or data["error"]
